9dd7a5342d8793da74f452030294db63309c6d7c
chuganghong <chuganghong@gmail.com>
2014-04-24 21:01:42 +0800

    some SQL code
    transaction

--- sp-variables.sql
+++ sp-variables.sql
@@ -3,17 +3,22 @@
 DELIMITER //
 DROP FUNCTION IF EXISTS variable_example //
 CREATE FUNCTION variable_example (seed INT)
-RETURN INT
+RETURNS INT
+
+DETERMINISTIC  
+READS SQL DATA  
+
 BEGIN
 	DECLARE example_int INT;
 	DECLARE example_result INT;
 	
 	SET @example_int=1;
 
-	SELECT example_int + sedd INTO example_result;
+	SELECT (example_int + seed) INTO example_result;
 
 	RETURN example_result;
 
-END//
+END //
 
-DELIMITER;
+
+DELIMITER ;
